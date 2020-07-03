fa43bae4a55da955e6deb6a17caa692080b685e7
favor-ui <fervplore@gmail.com>
2020-07-03 17:10:08 +0100

    initial commit

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # creating a configuration class
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-can-guess'
-    MONGO_URI = os.environ.get('MONGO_URI') or  "mongodb+srv://favor:<mikehelpme>@cluster0.fbpmq.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    MONGO_URI = os.environ.get('MONGO_URI') or  os.environ.get("MONGO_URI")
